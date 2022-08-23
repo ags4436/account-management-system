b36142e3b931adc4069003f3d220b4824f2d7463
ags4436 <kvnaakash01@gmail.com>
2022-08-23 10:33:07 +0530

    AppRename

--- src/main/java/com/barclays/capstone/main/App.java
+++ src/main/java/com/barclays/capstone/main/App.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class AccountManagementSystemApplication {
+public class App {
 
 	public static void main(String[] args) {
-		SpringApplication.run(AccountManagementSystemApplication.class, args);
+		SpringApplication.run(App.class, args);
 	}
 
 }
