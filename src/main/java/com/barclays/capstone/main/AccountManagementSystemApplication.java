402089b5479b9d5ec1a72ce04e70b35ec3416ac0
ags4436 <kvnaakash01@gmail.com>
2022-08-23 10:22:10 +0530

    package-init

--- src/main/java/com/barclays/capstone/main/AccountManagementSystemApplication.java
+++ src/main/java/com/barclays/capstone/main/AccountManagementSystemApplication.java
@@ -1,4 +1,4 @@
-package accountmanagementsystem.comaccountmanagementsystem;
+package com.barclays.capstone.main;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
