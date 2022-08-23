509dcadf0ae522520f96a6b46e9a87b5c482e2fd
ags4436 <kvnaakash01@gmail.com>
2022-08-23 10:09:12 +0530

    init

---
+++ src/main/java/accountmanagementsystem/comaccountmanagementsystem/AccountManagementSystemApplication.java
@@ -0,0 +1,13 @@
+package accountmanagementsystem.comaccountmanagementsystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AccountManagementSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AccountManagementSystemApplication.class, args);
+	}
+
+}
---
+++ src/test/java/accountmanagementsystem/comaccountmanagementsystem/AccountManagementSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package accountmanagementsystem.comaccountmanagementsystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AccountManagementSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
