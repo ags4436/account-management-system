0c204572831472f9af022556ac1565ed35f130f4
Akash <akashsalave@gmail.com>
2022-08-24 14:07:20 +0530

    v2

--- src/main/java/com/barclays/capstone/main/service/BankServices.java
+++ src/main/java/com/barclays/capstone/main/service/BankServices.java
@@ -320,5 +320,42 @@ public class BankServices {
 		response.put("statusCode", "201");
 		return response;
 	}
+	
+	public boolean deposit(String accountNumber, int amount) {
+		if (repo.deposit(accountNumber, amount, generateTransactionId())) {
+			return true;
+		} else {
+			return false;
+		}
+	}
+
+	public boolean cashWithdrawal(String accountNumber, int amount) {
+		if (repo.cashWithdrawal(accountNumber, amount, generateTransactionId())) {
+			return true;
+		} else {
+			return false;
+		}
+	}
+	public String transfer(String fromAccount, String toAccount, int amount) {
+		String transactionId=generateTransactionId();
+		String result="";
+		if(repo.transfer(fromAccount, toAccount, amount,transactionId )) {
+			result="Transfering funds is successfull. TransactionId "+ transactionId;
+		}else {
+			result="Transfering fund is not done due to some error.Please try again";
+		}
+		return result;
+		
+	}
+	public String generateTransactionId() {
+        int min = 10000;
+        int max = 99999;
+
+        int partTransactionId = (int) (Math.random() * (max - min + 1) + min);
+        int SecondPartTransactionId = (int) (Math.random() * (max - min + 1) + min);
+
+        return String.valueOf(partTransactionId)+String.valueOf(SecondPartTransactionId);
+    }
+
 
 }
