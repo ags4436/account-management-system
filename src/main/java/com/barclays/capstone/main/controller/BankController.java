68bc36d77e6bf9dbb7f26bf733fa6f1f752ef891
Akash <akashsalave@gmail.com>
2022-08-24 09:40:13 +0530

    v1

--- src/main/java/com/barclays/capstone/main/controller/BankController.java
+++ src/main/java/com/barclays/capstone/main/controller/BankController.java
@@ -9,9 +9,11 @@ import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.barclays.capstone.main.model.BankCustomer;
@@ -71,6 +73,57 @@ public class BankController {
 		return operations.createAccount(customer);
 	}
 	
-	
+	@PutMapping("/deposit")
+	public ResponseEntity<String> cashDeposit(@RequestParam String accountNumber, @RequestParam int amount) {
+		String result;
+		ResponseEntity<String> response;
+		logger.info("depositing.................");
+		if (operations.deposit(accountNumber, amount)) {
+			result = amount + " is successfully deposited to account " + accountNumber;
+			logger.info(amount+" is deposited to account: "+accountNumber);
+			response = new ResponseEntity<String>(result, HttpStatus.OK);
+		} else {
+			logger.info("Error.....................");
+			result = "Amount is not deposited due to some error please try again";
+			response = new ResponseEntity<String>(result, HttpStatus.BAD_REQUEST);
+		}
+		return response;
+	}
+	@PutMapping("/withdrawal")
+	public ResponseEntity<String> cashWithdrawal(@RequestParam String accountNumber, @RequestParam int amount) {
+		String result;
+		ResponseEntity<String> response;
+		logger.info("Withdrawing the money.....................");
+		if(amount>10000) {
+			logger.info("Cannot withdraw more than 10000");
+			result="Limit for withdraw in single day is 10000. More than 10000 cannot be withdraw in single day";
+			response=new ResponseEntity<String>(result,HttpStatus.BAD_REQUEST);
+		}
+		if (operations.cashWithdrawal(accountNumber, amount)) {
+			result = amount + " withdraw from account " + accountNumber;
+			logger.info(amount+" has been withdraw from acoount: "+accountNumber);
+			response = new ResponseEntity<String>(result, HttpStatus.OK);
+		} else {
+			logger.info("Error......cannot withdraw");
+			result = "cash Withdrawal can not be done due to some error. Please try again";
+			response = new ResponseEntity<String>(result, HttpStatus.BAD_REQUEST);
+		}
+		return response;
+
+	}
+	@PutMapping("/transfer")
+	public ResponseEntity<String> transfer(@RequestParam String fromAccountNumber, @RequestParam String toAccountNumber,
+			@RequestParam int amount) {
+		logger.info("Transferring....................");
+		String result= operations.transfer(fromAccountNumber, toAccountNumber, amount);
+		if(result.contains("successfull")) {
+			logger.info("Funds are transferred successfull");
+			return new ResponseEntity<String>(result,HttpStatus.OK);
+		}else {
+			logger.info("Funds are not transfered successfully. Please try again");
+			return new ResponseEntity<String>(result,HttpStatus.BAD_REQUEST);
+		}
+	}
 
 }
+	
--- src/main/java/com/barclays/capstone/main/service/BankServices.java
+++ src/main/java/com/barclays/capstone/main/service/BankServices.java
@@ -83,5 +83,41 @@ public class BankServices {
 		email.sendEmail(customer.getEmail(),"","");
 		return accountNumber;
 	}
+	
+	public boolean deposit(String accountNumber, int amount) {
+		if (repo.deposit(accountNumber, amount, generateTransactionId())) {
+			return true;
+		} else {
+			return false;
+		}
+	}
+
+	public boolean cashWithdrawal(String accountNumber, int amount) {
+		if (repo.cashWithdrawal(accountNumber, amount, generateTransactionId())) {
+			return true;
+		} else {
+			return false;
+		}
+	}
+	public String transfer(String fromAccount, String toAccount, int amount) {
+		String transactionId=generateTransactionId();
+		String result="";
+		if(repo.transfer(fromAccount, toAccount, amount,transactionId )) {
+			result="Transfering funds is successfull. TransactionId "+ transactionId;
+		}else {
+			result="Transfering fund is not done due to some error.Please try again";
+		}
+		return result;
+		
+	}
+	public String generateTransactionId() {
+        int min = 10000;
+        int max = 99999;
+
+        int partTransactionId = (int) (Math.random() * (max - min + 1) + min);
+        int SecondPartTransactionId = (int) (Math.random() * (max - min + 1) + min);
+
+        return String.valueOf(partTransactionId)+String.valueOf(SecondPartTransactionId);
+    }
 
 }
