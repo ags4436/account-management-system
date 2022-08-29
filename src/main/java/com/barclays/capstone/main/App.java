017e9ea2d00519942ea5a6eb6c28085dfa6e71c4
V Aakash Gouri Shankar <41334413+ags4436@users.noreply.github.com>
2022-08-29 10:54:43 +0530

    Add SpringBootServletInitializer

--- src/main/java/com/barclays/capstone/main/App.java
+++ src/main/java/com/barclays/capstone/main/App.java
@@ -2,6 +2,8 @@ package com.barclays.capstone.main;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.web.servlet.HandlerInterceptor;
 
 /**
@@ -12,10 +14,14 @@ import org.springframework.web.servlet.HandlerInterceptor;
  */
 
 @SpringBootApplication
-public class App implements HandlerInterceptor {
-
-	public static void main(String[] args) {
-		SpringApplication.run(App.class, args);
-	}
-
+public class App extends SpringBootServletInitializer{
+ 
+    public static void main(String[] args) {
+        SpringApplication.run(App.class, args);
+    }
+ 
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+        return builder.sources(App.class);
+    }
 }
